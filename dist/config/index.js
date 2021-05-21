3fcea3c94ba8ebc6281ac47461339e68cc208f09
NoamYes <noamiko600@gmail.com>
2021-05-21 11:39:47 +0300

    Init commit with files from template

---
+++ dist/config/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const env = require("env-var");
+require("./dotenv");
+const config = {
+    service: {
+        port: env.get('PORT').required().asPortNumber(),
+    },
+    mongo: {
+        uri: env.get('MONGO_URI').required().asUrlString(),
+        featureCollectionName: env.get('MONGO_FEATURE_COLLECTION_NAME').required().asString(),
+    },
+    rabbit: {
+        uri: env.get('RABBIT_URI').required().asUrlString(),
+        retryOptions: {
+            minTimeout: env.get('RABBIT_RETRY_MIN_TIMEOUT').default(1000).asIntPositive(),
+            retries: env.get('RABBIT_RETRY_RETRIES').default(10).asIntPositive(),
+            factor: env.get('RABBIT_RETRY_FACTOR').default(1.8).asFloatPositive(),
+        },
+    },
+};
+exports.default = config;
+//# sourceMappingURL=index.js.map

2618d65d76e7a527cfbed28065282c562bc03207
NoamYes <noamiko600@gmail.com>
2021-05-26 09:38:22 +0300

    (Add) configuration files

---
+++ src/entity/entity.interface.ts
@@ -0,0 +1,29 @@
+import { Types } from 'mongoose';
+
+export interface IEntity {
+    // Person's Basic information
+    id: string;
+    displayName: string;
+    entityType: string; // enum
+    identityCard: string;
+    personalNumber: string;
+    serviceType: string;
+    firstName: string;
+    lastName: string;
+    akaUnit: string;
+    status: string;
+
+    dischargeDate: Date;
+    rank: string; // enum
+    mail: string;
+    job: string;
+    phone: string;
+    mobilePHone: string;
+    address: string;
+    clearance: string; // string of number - enum
+    pictures?: {
+        profile?: ProfilePictureDTO;
+    };
+    sex?: string;
+    birthDate?: Date;
+}

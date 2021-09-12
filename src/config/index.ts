d114f32b7e5069c45c599f47df25c2a6da14c969
NoamYes <noamiko600@gmail.com>
2021-09-12 12:56:18 +0300

    fix: remove unnecessary in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,21 +6,13 @@ const config = {
         port: env.get('PORT').required().asPortNumber(),
     },
     mongo: {
-        uri: env.get('MONGO_URI').required().asUrlString(),
+        uri: env.get('MONGO_URI').required().asString(),
         EntityCollectionName: env.get('ENTITY_COLLECTION_NAME').required().asString(),
         EntityDenormalizedCollectionName: env.get('ENTITY_DENORMALIZED_COLLECTION_NAME').required().asString(),
         DigitalIdentityCollectionName: env.get('DI_COLLECTION_NAME').required().asString(),
         DigitalIdentityDenormalizedCollectionName: env.get('DI_DENORMALIZED_COLLECTION_NAME').required().asString(),
         RoleCollectionName: env.get('ROLE_COLLECTION_NAME').required().asString(),
         GroupCollectionName: env.get('OG_COLLECTION_NAME').required().asString(),
-    },
-    rabbit: {
-        uri: env.get('RABBIT_URI').required().asUrlString(),
-        retryOptions: {
-            minTimeout: env.get('RABBIT_RETRY_MIN_TIMEOUT').default(1000).asIntPositive(),
-            retries: env.get('RABBIT_RETRY_RETRIES').default(10).asIntPositive(),
-            factor: env.get('RABBIT_RETRY_FACTOR').default(1.8).asFloatPositive(),
-        },
     },
 };
 

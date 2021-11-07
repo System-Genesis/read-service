ccf47cd1b6dd5368272a63e9255b6967b53ffac7
NoamYes <noamiko600@gmail.com>
2021-11-07 13:54:52 +0200

    (fix): digitalIdentity.source query

--- src/entity/entity.validator.schema.ts
+++ src/entity/entity.validator.schema.ts
@@ -78,7 +78,7 @@ export const getEntitiesByCustomFilters = getRequestBaseSchema.keys({
         ids: Joi.array().items(Joi.objectId()),
         rank: Joi.alternatives().try(Joi.array(), Joi.string()), // TODO: test rank array or string
         entityType: Joi.string(),
-        'digitalIdentities.source': Joi.alternatives().try(Joi.array(), Joi.string()),
+        'digitalIdentity.source': Joi.alternatives().try(Joi.array(), Joi.string()),
         status: Joi.string(),
         updatedFrom: Joi.date().iso(),
         pageNum: Joi.number().min(1),
